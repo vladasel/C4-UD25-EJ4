5cfeb4f12502b3e559e4a732c05f915fe45a51d7
vladasel <ionelavladasel2710@gmail.com>
2022-08-22 15:13:43 +0200

    controllers done

--- src/main/java/com/ionela/rest/controller/PeliculaController.java
+++ src/main/java/com/ionela/rest/controller/PeliculaController.java
@@ -1,5 +1,68 @@
 package com.ionela.rest.controller;
 
-public class PeliculaController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ionela.rest.dto.Pelicula;
+import com.ionela.rest.service.PeliculaServiceImpl;
+
+@RestController
+@RequestMapping("/api")
+public class PeliculaController {
+	@Autowired
+	PeliculaServiceImpl peliculaServiceImpl;
+
+	@GetMapping("/pelicuals")
+	public List<Pelicula> listarPeliculas() {
+		return peliculaServiceImpl.listarPeliculas();
+	}
+
+	@PostMapping("/peliculas")
+	public Pelicula salvarPelicula(@RequestBody Pelicula pelicula) {
+
+		return peliculaServiceImpl.guardarPelicula(pelicula);
+	}
+
+	@GetMapping("/peliculas/{id}")
+	public Pelicula peliculasXID(@PathVariable(name = "id") Long id) {
+
+		Pelicula peli_xid = new Pelicula();
+
+		peli_xid = peliculaServiceImpl.peliculaXID(id);
+
+		System.out.println("Departamento XID: " + peli_xid);
+
+		return peli_xid;
+	}
+
+	@PutMapping("/peliculas/{id}")
+	public Pelicula actualizarPelicula(@PathVariable(name = "id") Long id, @RequestBody Pelicula pelicula) {
+
+		Pelicula peli_seleccionado = new Pelicula();
+		Pelicula peli_actualizado = new Pelicula();
+
+		peli_seleccionado = peliculaServiceImpl.peliculaXID(id);
+
+		peli_seleccionado.setNombre(pelicula.getNombre());
+		peli_seleccionado.setCalificacion_edad(pelicula.getCalificacion_edad());
+
+		peli_actualizado = peliculaServiceImpl.actualizarPelicula(peli_seleccionado);
+
+		return peli_actualizado;
+	}
+
+	@DeleteMapping("/peliculas/{id}")
+
+	public void eleiminarPelicula(@PathVariable(name = "id") Long id) {
+		peliculaServiceImpl.eliminarPelicula(id);
+	}
 }
--- src/main/java/com/ionela/rest/controller/SalaController.java
+++ src/main/java/com/ionela/rest/controller/SalaController.java
@@ -1,5 +1,68 @@
 package com.ionela.rest.controller;
 
-public class SalaController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ionela.rest.dto.Sala;
+import com.ionela.rest.service.SalaServiceImpl;
+
+@RestController
+@RequestMapping("/api")
+public class SalaController {
+	@Autowired
+	SalaServiceImpl salaServiceImpl;
+
+	@GetMapping("/salas")
+	public List<Sala> listarDepartamentos() {
+		return salaServiceImpl.listarSalas();
+	}
+
+	@PostMapping("/salas")
+	public Sala salvarSala(@RequestBody Sala sala) {
+
+		return salaServiceImpl.guardarSala(sala);
+	}
+
+	@GetMapping("/salas/{id}")
+	public Sala salaXID(@PathVariable(name = "id") Long id) {
+
+		Sala sala_xid = new Sala();
+
+		sala_xid = salaServiceImpl.salaXID(id);
+
+		System.out.println("Sala XID: " + sala_xid);
+
+		return sala_xid;
+	}
+
+	@PutMapping("/salas/{id}")
+	public Sala actualizarSala(@PathVariable(name = "id") Long id, @RequestBody Sala sala) {
+
+		Sala sala_seleccionado = new Sala();
+		Sala sala_actualizado = new Sala();
+
+		sala_seleccionado = salaServiceImpl.salaXID(id);
+
+		sala_seleccionado.setNombre(sala.getNombre());
+		sala_seleccionado.setPelicula(sala.getPelicula());
+
+		sala_actualizado = salaServiceImpl.actualizarSala(sala_seleccionado);
+
+		return sala_seleccionado;
+	}
+
+	@DeleteMapping("/salas/{id}")
+
+	public void eleiminarSala(@PathVariable(name = "id") Long id) {
+		salaServiceImpl.eliminarSala(id);
+	}
 }
