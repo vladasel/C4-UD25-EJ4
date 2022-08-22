1671b11ab7e73c7c6e2942111bb90ca3bb14f36a
vladasel <ionelavladasel2710@gmail.com>
2022-08-22 14:54:29 +0200

    done interface Pelicula and Sala

--- src/main/java/com/ionela/rest/service/IPeliculaService.java
+++ src/main/java/com/ionela/rest/service/IPeliculaService.java
@@ -1,5 +1,21 @@
 package com.ionela.rest.service;
 
+import java.util.List;
+
+import com.ionela.rest.dto.Pelicula;
+
+
+
 public interface IPeliculaService {
 
+	//Metodos del CRUD
+		public List<Pelicula> listarPeliculas(); //Listar All 
+		
+		public Pelicula guardarPelicula(Pelicula pelicula);	//Guarda una pelicula CREATE
+		
+		public Pelicula peliculaXID(Long id); //Leer datos de una pelicula READ
+		
+		public Pelicula actualizarPelicula(Pelicula pelicula); //Actualiza datos de la peli UPDATE
+		
+		public void eliminarPelicula(Long id);// Elimina l apeli DELETE
 }
--- src/main/java/com/ionela/rest/service/ISalaService.java
+++ src/main/java/com/ionela/rest/service/ISalaService.java
@@ -1,5 +1,19 @@
 package com.ionela.rest.service;
 
+import java.util.List;
+
+import com.ionela.rest.dto.Sala;
+
 public interface ISalaService {
 
+	// Metodos del CRUD
+	public List<Sala> listarSalas(); // Listar All
+
+	public Sala guardarSala(Sala departamento); // Guarda un sala CREATE
+
+	public Sala salaXID(Long id); // Leer datos de un sala READ
+
+	public Sala actualizarSala(Sala sala); // Actualiza datos de sala UPDATE
+
+	public void eliminarSala(Long id);// Elimina la sala DELETE
 }
